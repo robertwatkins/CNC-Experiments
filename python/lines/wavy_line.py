cc75658fb3dc2360044ffdd00ec274b9562d09f8
Robert Watkins <robert.watkins@sonicdrivein.com>
2024-06-30 11:32:26 -0400

    added module, reorganized files and updated readme

---
+++ python/lines/wavy_line.py
@@ -0,0 +1,34 @@
+import gcode
+import math
+
+tools = []
+tools.append(gcode.tool(2,"90deg 5mm Conical"))
+this_machine = gcode.machine("MillRite","GRBL", \
+                             ["T2 M6;","G0  Z1.0000 G43 H1;","G90;","S2000 M3;","G21;","G0 Z5;","G0 X100 Y100 F100;"], \
+                             ["G1 Z-2.5;","G1 X200 Y100 F100;","M5;","G0 Z30;"])
+
+project = gcode.project(tools, this_machine)
+
+start_depth = 0
+end_depth = -2.5
+steps = 1000  # Number of steps for interpolation
+x_increment = 20 / steps
+#y_increment = 10 / steps
+#z_increment = (end_depth - start_depth) / steps
+
+for i in range(1, steps + 1):
+    x = i * x_increment
+    y = 0
+    z = math.cos((i-1)/100) - 1
+    command = f"G1 X{x:.2f} Y{y:.2f} Z{z:.2f} F100"
+    project.commands.append(command)
+
+project.finalize()
+
+#show all details of the project
+#print(project)
+
+#show just the resulting gcode
+for command in project.commands:
+    print(command)
+
--- readme.md
+++ readme.md
@@ -13,3 +13,8 @@ https://camotics.org
 ### python
 This folder contains scripts I'm using to generate the gcode
 to put through the simulator
+
+### python/lines
+I'm starting a module called 'gcode' to manage the production of a gcode file.
+
+The 'deepening_line_with_module.py' is an initial use of that module
